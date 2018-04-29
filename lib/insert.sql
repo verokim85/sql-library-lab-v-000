bb66d15630a2dde9e47720beee2e9d97eeed55c8
Veronica Kim <vero.a.kim@gmail.com>
2018-04-29 02:11:06 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("title", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("title", 1, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+INSERT INTO books (title, year, series_id) VALUES ("title", 2018, 1);
+
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+INSERT INTO characters (name, species, motto, author_id, series_id) VALUES ("A", "B", "C", 1, 2);
+
+INSERT INTO subgenres (name) VALUES ("D");
+INSERT INTO subgenres (name) VALUES ("D");
+
+INSERT INTO authors (name) VALUES ("name");
+INSERT INTO authors (name) VALUES ("name");
+
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,46 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+  WHERE series_id = 1
+  ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+    SELECT name, motto
+    FROM characters
+    ORDER BY LENGTH(motto) DESC
+    LIMIT 1
+  SQL
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*)
+    FROM characters
+    GROUP BY species
+    ORDER BY COUNT(species) DESC
+    LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+   FROM authors INNER JOIN subgenres
+   ON authors.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+    FROM characters
+    JOIN series ON series.id = characters.id
+    GROUP BY species
+    ORDER BY COUNT(species) DESC
+    LIMIT 1 "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) AS book_count
+    FROM character_books
+    JOIN characters
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY book_count DESC"
 end
